2ca8c31c2dbc74b2321d080a92147bdf9f3f4364
Vsevolod Donchenko <vsevolond@gmail.com>
2024-10-29 20:48:48 +0300

    PROD: started

---
+++ Phenakistiscope-iOS/Phenakistiscope-iOS/Phenakistiscope_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phenakistiscope_iOSApp.swift
+//  Phenakistiscope-iOS
+//
+//  Created by Vsevolod Donchenko on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Phenakistiscope_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
